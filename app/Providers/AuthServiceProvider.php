e1323302481a2acb37330cf787ce8d8a9bcabb4c
KingRayhan <rayhan095@gmail.com>
2021-04-04 17:01:29 +0600

    password reset done

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        ResetPassword::createUrlUsing(function ($user, $token) {
+            return env('CLIENT_URL') . '/auth/reset-password?email=' . $user->email . '&token=' . $token;
+        });
     }
 }
