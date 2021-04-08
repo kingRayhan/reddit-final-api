1b3d46af18a6e4f78adfde26739b0e6d09f8c0a8
KingRayhan <rayhan095@gmail.com>
2021-04-08 16:17:21 +0600

    email template modified

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img
+                src="https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/Reddit-512.png"
+                class="logo"
+                alt="Laravel Logo">
+        </a>
+    </td>
+</tr>
